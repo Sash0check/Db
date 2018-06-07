303ccbb79732d0d903cc4800ec4d4fad89c182ca
Sash0check <Sanek_015@mail.ru>
2018-06-07 20:02:17 +0800

    Add files via upload

---
+++ tables_create.sql
@@ -0,0 +1,82 @@
+CREATE TABLE `books` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` varchar(100) NOT NULL,
+	`extra_information` varchar(255) NOT NULL,
+	`avalibality` BOOLEAN NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `delivery` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`client_id` INT NOT NULL,
+	`date_of_delivery` DATE NOT NULL,
+	`date_of_return` DATE NOT NULL,
+	`worker_id` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `clients` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` varchar(50) NOT NULL,
+	`lastname` varchar(50) NOT NULL,
+	`patronymic` varchar(50) NOT NULL,
+	`dbirth` DATE NOT NULL,
+	`adress` varchar(50) NOT NULL,
+	`phone_numder` varchar(50) NOT NULL,
+	`email` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `workers` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` varchar(50) NOT NULL,
+	`lastname` varchar(50) NOT NULL,
+	`phone_numder` varchar NOT NULL,
+	`adress` varchar NOT NULL,
+	`dbirth` varchar NOT NULL,
+	`email` varchar NOT NULL,
+	`patronymic` varchar(50) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `authors` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`firstname` varchar(50) NOT NULL,
+	`secondname` varchar(50) NOT NULL,
+	`patronymic` varchar(50) NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `genre` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`name` varchar NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `book_authors` (
+	`book_id` INT NOT NULL,
+	`author_id` INT NOT NULL,
+	PRIMARY KEY (`book_id`,`author_id`)
+);
+
+CREATE TABLE `book_delivery` (
+	`delivery_id` INT NOT NULL,
+	`book_id` INT NOT NULL,
+	PRIMARY KEY (`delivery_id`,`book_id`)
+);
+
+CREATE TABLE `fines` (
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`delivery_id` INT NOT NULL,
+	`description` varchar NOT NULL,
+	`pice` INT NOT NULL,
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `book_genre` (
+	`book_id` INT NOT NULL,
+	`genre_id` INT NOT NULL,
+	PRIMARY KEY (`book_id`,`genre_id`)
+);
+
+
