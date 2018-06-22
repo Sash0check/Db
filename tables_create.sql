3d76e947760203fb2a051a774d1607f24aeb45f3
Sash0check <Sanek_015@mail.ru>
2018-06-22 21:57:36 +0800

    Update tables_create.sql

--- tables_create.sql
+++ tables_create.sql
@@ -2,7 +2,7 @@ CREATE TABLE `books` (
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`name` varchar(100) NOT NULL,
 	`extra_information` varchar(255) NOT NULL,
-	`avalibality` BOOLEAN NOT NULL,
+	`availability` BOOLEAN NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
