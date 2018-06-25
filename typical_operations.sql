8b1a765b37161a3ade1c967d473d6d0a2bf53f58
Sash0check <Sanek_015@mail.ru>
2018-06-25 22:49:22 +0800

    Update typical_operations.sql

--- typical_operations.sql
+++ typical_operations.sql
@@ -1,20 +1,20 @@
 /*Добавление автора*/
-INSERT INTO `authors` (`firstname`, `secondname`, `patronymic`) VALUES ('Михаил', 'Булгаков', 'Афанасьевич');
+INSERT INTO `authors` (`firstname`, `lastname`, `patronymic`) VALUES ('Михаил', 'Булгаков', 'Афанасьевич');
 
 /*Добавление жанра*/
 INSERT INTO `genre` (`name`) VALUES ('Мистицизм');
 
 /*Добавление работника*/
-INSERT INTO `workers` (`name`, `lastname`, `phone_numder`, `adress`, `dbirth`, `email`, `patronymic`) 
+INSERT INTO `workers` (`firstname`, `lastname`, `phone_numder`, `adress`, `dbirth`, `email`, `patronymic`) 
 VALUES ('Александр', 'Абдулов', '89149082004', 'Свердлова 25', '1998-07-15', 'sanek_015@mail.ru', 'Алексеевич')
 
 /*Добавление клиента*/
-INSERT INTO `clients` ( `name`, `lastname`, `patronymic`, `dbirth`, `adress`, `phone_numder`, `email`) 
+INSERT INTO `clients` ( `firstname`, `lastname`, `patronymic`, `dbirth`, `adress`, `phone_numder`, `email`) 
 VALUES ('Иван', 'Петров', 'Сергеевич', '1988-12-12', 'где-то в России', '88005553535', 'temp@tempmail.ru')
 
 
 /*Добавление книги*/
-INSERT INTO `books` (`name`, `extra_information`, `avalibality`) VALUES ('Мастер и Маргарита', 'издание **', '1');
+INSERT INTO `books` (`name`, `extra_information`, `availability`) VALUES ('Мастер и Маргарита', 'издание **', '1');
 
 /*Определение автора*/
 INSERT INTO `book_authors` (`book_id`, `author_id`) VALUES ('1', '1');
@@ -33,4 +33,4 @@ INSERT INTO `delivery` (`id`, `client_id`, `date_of_delivery`, `date_of_return`,
 INSERT INTO `book_delivery` (`delivery_id`, `book_id`) VALUES ('1', '1');
 
 /*Обновление доступности книги*/
-UPDATE `books` SET `avalibality` = 0 WHERE `id` = 1;
+UPDATE `books` SET `availability` = 0 WHERE `id` = 1;
