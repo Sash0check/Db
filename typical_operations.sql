40330c461e3935c0378e91b33d36969de615cef9
Sash0check <Sanek_015@mail.ru>
2018-06-25 23:56:19 +0800

    Update typical_operations.sql

--- typical_operations.sql
+++ typical_operations.sql
@@ -2,7 +2,7 @@
 INSERT INTO `authors` (`firstname`, `lastname`, `patronymic`) VALUES ('Михаил', 'Булгаков', 'Афанасьевич');
 
 /*Добавление жанра*/
-INSERT INTO `genre` (`name`) VALUES ('Мистицизм');
+INSERT INTO `genres` (`name`) VALUES ('Мистицизм');
 
 /*Добавление работника*/
 INSERT INTO `workers` (`firstname`, `lastname`, `phone_numder`, `adress`, `dbirth`, `email`, `patronymic`) 
@@ -17,20 +17,20 @@ VALUES ('Иван', 'Петров', 'Сергеевич', '1988-12-12', 'где-
 INSERT INTO `books` (`name`, `extra_information`, `availability`) VALUES ('Мастер и Маргарита', 'издание **', '1');
 
 /*Определение автора*/
-INSERT INTO `book_authors` (`book_id`, `author_id`) VALUES ('6', '6');
+INSERT INTO `books_authors` (`book_id`, `author_id`) VALUES ('6', '6');
 
 /*Определение жанра 1*/
-INSERT INTO `book_genre` (`book_id`, `genre_id`) VALUES ('6', '1');
+INSERT INTO `books_genres` (`book_id`, `genre_id`) VALUES ('6', '1');
 
 /*Определение жанра 2*/
-INSERT INTO `book_genre` (`book_id`, `genre_id`) VALUES ('6', '2');
+INSERT INTO `books_genres` (`book_id`, `genre_id`) VALUES ('6', '2');
 
 
 /*Добавление заказа*/
-INSERT INTO `delivery` (`id`, `client_id`, `date_of_delivery`, `date_of_return`, `worker_id`) VALUES (NULL, '1', '2018-06-07', '2018-06-08', '1');
+INSERT INTO `deliverys` (`id`, `client_id`, `date_of_delivery`, `date_of_return`, `worker_id`) VALUES (NULL, '1', '2018-06-07', '2018-06-08', '1');
 
 /*Определение книги*/
-INSERT INTO `book_delivery` (`delivery_id`, `book_id`) VALUES ('1', '1');
+INSERT INTO `books_deliverys` (`delivery_id`, `book_id`) VALUES ('1', '1');
 
 /*Обновление доступности книги*/
 UPDATE `books` SET `availability` = 0 WHERE `id` = 1;
